2a03741feed4c4e420b50a1b9cffde293df623a6
vema17 <vema1706@gmail.com>
2024-11-07 11:49:02 -0300

    carpeta para pagina principal aun sin implementar

--- home/app.js
+++ home/app.js
@@ -1,27 +1,22 @@
-const express = require('express');
+/*const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const userRoutes = require('./routes/userRoutes');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 // Middleware
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
-app.use('/uploads', express.static('/app/uploads'));
 
 // Ruta para servir el archivo HTML en la página principal
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
-// Rutas protegidas 
-app.use('/api/users', userRoutes);
-
 app.listen(port, () => {
-  console.log(`Servicio de usuario corriendo en http://localhost:${port}`);
-});
+  console.log(`Pagina principal corriendo en http://localhost:${port}`);
+}); */
