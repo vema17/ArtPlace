707bf48229196ddf2392b18bd30187dff3ccdfc7
Vicente Muñoz <vicente.munoz@userena.cl>
2024-11-10 22:48:35 -0300

    Reapply "update"
    This reverts commit 8fe31bcd8e343a597a9ebcae518b7e07b13a610a.

--- product-service/src/middleware/productConsumer.js
+++ product-service/src/middleware/productConsumer.js
@@ -1,33 +1,26 @@
-const amqp = require('amqplib');
-const RABBITMQ_URL = process.env.RABBITMQ_URL || 'amqp://localhost:5672';
+const { consumeMessages } = require('../config/rabbitmqService');
 
 let currentUserId = null; // Variable para almacenar el ID del usuario actual
 
-async function connectAndConsume() {
-    const connection = await amqp.connect(RABBITMQ_URL);
-    const channel = await connection.createChannel();
-    
-    // Asegura la cola y el intercambio, y realiza la vinculación
-    await channel.assertExchange('marketplace-exchange', 'direct', { durable: true });
-    await channel.assertQueue('usuarios-to-productos', { durable: true });
-    await channel.bindQueue('usuarios-to-productos', 'marketplace-exchange', 'userToProduct');
-    
-    console.log('Consumiendo mensajes de usuarios-to-productos');
-    
-    // Consume mensajes de la cola `usuarios-to-productos`
-    channel.consume('usuarios-to-productos', (msg) => {
-        if (msg) {
-            const message = JSON.parse(msg.content.toString());
-            if (message.action === 'user_login') {
-                currentUserId = message.userId;
-                console.log(`User ID ${currentUserId} almacenado en el consumidor de productos.`);
-            }
-            channel.ack(msg);
-        }
-    });
+function handleUserLogin(message) {
+    if (message.action === 'user_login' && message.userId) {
+        currentUserId = message.userId;
+        console.log(`User ID ${currentUserId} almacenado en el consumidor de productos.`);
+    } else {
+        console.log("Mensaje inválido o faltan campos necesarios:", message);
+    }
+}
+
+async function startConsuming() {
+    try {
+        await consumeMessages('usuarios-to-productos', handleUserLogin); // Consume mensajes desde la cola configurada
+        console.log('Consumo iniciado en la cola usuarios-to-productos');
+    } catch (error) {
+        console.error('Error al iniciar el consumo de mensajes:', error);
+    }
 }
 
 module.exports = {
-    connectAndConsume,
+    startConsuming,
     getCurrentUserId: () => currentUserId
 };
